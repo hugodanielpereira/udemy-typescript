273a00ed998073087842ec4aa4693a654b8b4cbc
Hugo Daniel Pereira <hugo-daniel.santos-pereira@capgemini.com>
2025-09-16 16:52:07 +0100

    Added the import fs from node

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import fs from 'node:fs';
+
+//fs.readFileSync();
+
+let userName: string;
+
+userName = 'Max';
+
+console.log(userName);
+
+function add(n1: any, n2: any) {
+  return n1 + n2;
+}
+
+console.log(add(5, 10));
